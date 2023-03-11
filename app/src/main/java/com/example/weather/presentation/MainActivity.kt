c3702e4fa53cd339228a70b062daac408d078e83
Art D <adpm@list.ru>
2023-03-11 21:46:56 +0600

    base update and configuration

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weather
+package com.example.weather.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.weather.presentation.screens.MainScreen
 import com.example.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainActivityScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherTheme {
-        Greeting("Android")
-    }
+fun MainActivityScreen() {
+    MainScreen()
 }
