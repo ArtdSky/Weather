71742688a2ebe8502ecd4c613737272228e780b6
Art D <adpm@list.ru>
2023-03-12 23:16:54 +0600

    test

--- app/src/main/java/com/example/weather/widget/WeatherWidget.kt
+++ app/src/main/java/com/example/weather/widget/WeatherWidget.kt
@@ -2,14 +2,28 @@ package com.example.weather.widget
 
 import androidx.compose.runtime.Composable
 import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
 import androidx.glance.text.Text
 
 class WeatherWidget: GlanceAppWidget() {
 
     @Composable
     override fun Content() {
-        Text(text = "Hello world!")
+        Column(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Hello world!")
+
+        }
     }
 }
 
+//• Текст с информацией о текущей температуре
+//• Текст с информацией о городе (определяется по геопозиции, с помощью готового API)
+//• Текст с информацией, когда было последнее обновление данных (UTC Time)
+//• Иконка с погодой в зависимости от погоды, можно реализовать только 3-4 сценария
+//погоды, все необязательно.
+//• Информация в виджете должна полностью синхронизироваться с выбранными
+//настройками в самом приложении!
